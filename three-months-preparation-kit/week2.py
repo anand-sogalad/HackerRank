be537c5c138f051cf5518bcc69061d6d9e1c48b3
Anand Sogalad <anand.sogalad305@gmail.com>
2025-04-06 19:35:57 +0530

    Add function to calculate diagonal difference in a matrix and update main to demonstrate usage

--- three-months-preparation-kit/week2.py
+++ three-months-preparation-kit/week2.py
@@ -23,6 +23,15 @@ def flipping_bits(n):
     return n ^ 0xFFFFFFFF
 
 
+def diagnal_difference(arr):
+    left_to_right = 0
+    right_to_left = 0
+    for idx, row in enumerate(arr):
+        left_to_right += row[idx]
+        right_to_left += row[-(idx + 1)]
+    return abs(left_to_right - right_to_left)
+
+
 def main():
     arr = [1, 2, 3, 4, 3, 2, 1]
     result = lonely_int(arr)
@@ -35,6 +44,10 @@ def main():
     number = 1
     result = flipping_bits(number)
     print(f"Result after flipping: {result}")
+
+    arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+    result = diagnal_difference(arr)
+    print(f"Diagnal difference: {result}")
 
 
 if __name__ == "__main__":
